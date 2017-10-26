f4a19535a6c4146bd12ca2d89075dac8bc89fc36
Ahmed Hady <ahmedhady6@gmail.com>
2017-10-26 22:08:04 +0200

    Initial commit

---
+++ build/soong/soong_config.mk
@@ -0,0 +1,10 @@
+# Insert new variables inside the Jdc structure
+aosp_soong:
+	$(hide) mkdir -p $(dir $@)
+	$(hide) (\
+	echo '{'; \
+	echo '"aosp": {'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false)'; \
+	echo '},'; \
+	echo '') > $(SOONG_VARIABLES_TMP)
---
+++ config/common.mk
@@ -0,0 +1,173 @@
+# Copyright (C) 2016 The JDCTeam
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# include definitions for SDCLANG
+# include vendor/aosp/sdclang/sdclang.mk
+
+include vendor/aosp/config/version.mk
+
+PRODUCT_BRAND ?= UltraDevsTeam
+
+# Use signing keys for user builds
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/aosp/.keys/release
+endif
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/aosp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/aosp/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+
+# Bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay/common \
+    vendor/aosp/overlay/dictionaries
+
+# Custom packages
+PRODUCT_PACKAGES += \
+    BluetoothExt \
+    Jelly \
+    LatinIME \
+    Launcher3 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    Stk \
+    Turbo \
+    Eleven \
+    Terminal \
+    SnapdragonCamera
+
+# Extra tools
+PRODUCT_PACKAGES += \
+    e2fsck \
+    mke2fs \
+    tune2fs \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs \
+    mkfs.ntfs \
+    fsck.ntfs \
+    mount.ntfs \
+    7z \
+    bzip2 \
+    curl \
+    lib7z \
+    powertop \
+    pigz \
+    tinymix \
+    unrar \
+    unzip \
+    zip \
+    libtinycompress_vendor \
+    libboringssl-compat \
+    libbson \
+    libshim_gui \
+    libstlport \
+    libncurses \
+    bash \
+    gdbserver \
+    htop \
+    libsepol \
+    micro_bench \
+    oprofiled \
+    sqlite3 \
+    strace \
+    vim \
+    wget
+
+
+# Openssh
+PRODUCT_PACKAGES += \
+    scp \
+    sftp \
+    ssh \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh
+
+# Exchange support
+PRODUCT_PACKAGES += \
+    Exchange2
+
+# Backup Services whitelist
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
+
+# For keyboard gesture typing
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinime.so
+#else
+#PRODUCT_COPY_FILES += \
+#    vendor/aosp/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinime.so
+#endif
+
+# init.d support
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
+
+# JDC-specific init file
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/init.local.rc:root/init.aosp.rc
+
+# Copy over added mimetype supported in libcore.net.MimeUtils
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Enable wireless Xbox 360 controller support
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
+    
+# Stagefright FFMPEG plugin
+PRODUCT_PACKAGES += \
+    libffmpeg_extractor \
+    libffmpeg_omx \
+    media_codecs_ffmpeg.xml
+
+# Changelog
+#PRODUCT_COPY_FILES += \
+#    vendor/aosp/Changelog.md:system/etc/Changelog.md
+
+# Needed by some RILs and for some gApps packages
+PRODUCT_PACKAGES += \
+    librsjni \
+    libprotobuf-cpp-full
+
+# Charger images
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Recommend using the non debug dexpreopter
+USE_DEX2OAT_DEBUG ?= false
+
+# Magisk
+ifeq ($(WITH_ROOT),true)
+ PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/magisk/Magisk.zip:install/magisk/Magisk.zip
+else
+ $(warning Root method is undefined, please use 'WITH_ROOT := true' to define it)
+endif
+
